b648d8949717c4e1669d5c89a25d1b24f153db14
enaibei <esthernaibei2020@gmail.com>
2022-04-16 11:10:26 +0300

    More queries

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,3 +1,25 @@
+-- Query all rows and columns from the table
+SELECT *
+FROM supermarket_sales;
+
+-- Query distinct rows from a table
 SELECT * 
 FROM supermarket_sales
 WHERE country =  'kenya' AND gender = 'Female';
+
+-- Selecting distinct values
+SELECT DISTINCT 'Country'
+FROM supermarket_sales;
+
+-- Sort result in ascending order
+SELECT product_line, Country, gross_income 
+FROM supermarket_sales
+ORDER BY gross_income ASC; -- Or DESC for Descending
+
+
+
+
+
+
+
+
